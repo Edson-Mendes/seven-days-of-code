0000beaa9d3491906d773ca3018d0cc2f87bc553
Edson Mendes <edson.luiz.mendes@hotmail.com>
2022-08-09 16:52:54 -0300

    Dia #1 | Consumir API do IMDb

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1 align="center"> 7 Days of Code </h1>
+
+![Badge Concluído](https://img.shields.io/static/v1?label=Status&message=Em+Desenvolvimento&color=yellow&style=for-the-badge)
+![Badge Java](https://img.shields.io/static/v1?label=Java&message=17&color=orange&style=for-the-badge&logo=java)
+![Badge Maven](https://img.shields.io/static/v1?label=Maven&message=v3.8.4&color=critical&style=for-the-badge&logo=apache+maven)
+
+## :book: Resumo do projeto
+Projeto com o objetivo de consumir uma API de filmes do IMDB e criar uma página HTML de exibição das informações recebidas.
--- src/main/java/br/com/emendes/App.java
+++ src/main/java/br/com/emendes/App.java
@@ -1,9 +1,28 @@
 package br.com.emendes;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class App {
+  public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Digite sua api key: ");
+      String apiKey = input.nextLine();
+
+      try {
+          URI uri = new URI(String.format("https://imdb-api.com/en/API/Top250Movies/%s", apiKey));
+          HttpClient client = HttpClient.newHttpClient();
+          HttpRequest request = HttpRequest.newBuilder().uri(uri).build();
+
+          HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+          String body = response.body();
+
+          System.out.println(body);
+      } catch (Exception e) {
+          System.err.println("Não foi possivel fazer a requisição");
+      }
+  }
 }
